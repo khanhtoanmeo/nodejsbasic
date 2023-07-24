7b1bd21e1e599c7c182dcc9b9bfc4870b5d5b170
khanhtoanmeo <115925844+khanhtoanmeo@users.noreply.github.com>
2023-07-24 16:43:46 +0700

    first modification

--- nodejsBasicExcersie.js
+++ nodejsBasicExcersie.js
@@ -1,29 +1,39 @@
+const fetchData = async (url) => {
+  const request = await fetch(url);
+  return request.json();
+};
+
+const getPost = async (id) => {
+  const post = await fetchData(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  const commentsOfThePost = await fetchData(
+    `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+  );
+
+  return { ...post, comment: commentsOfThePost };
+};
+
 (async () => {
-  const usersRequest = await fetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  const postsRequest = await fetch(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  const commentsRequest = await fetch(
-    "https://jsonplaceholder.typicode.com/comments"
-  );
-  const users = await usersRequest.json();
-  const posts = await postsRequest.json();
-  const comments = await commentsRequest.json();
+  const [users, posts, comments] = await Promise.all([
+    fetchData("https://jsonplaceholder.typicode.com/users"),
+    fetchData("https://jsonplaceholder.typicode.com/posts"),
+    fetchData("https://jsonplaceholder.typicode.com/comments"),
+  ]);
 
   ///// 3
-  const usersFullInfomation = users.map((u) => {
-    const { id, name, username, email } = u;
+  const usersFullInfomation = users.map((user) => {
+    const { id, name, username, email } = user;
     const userToReturn = {
       id,
       name,
       username,
       email,
-      posts: posts.filter((p) => p.userId === u.id),
+      posts: posts.filter((post) => post.userId === user.id),
     };
     userToReturn.comments = userToReturn.posts.reduce(
-      (acc, cur) => acc.concat(comments.filter((c) => c.postId === cur.id)),
+      (acc, cur) =>
+        acc.concat(comments.filter((comment) => comment.postId === cur.id)),
       []
     );
     return userToReturn;
@@ -31,13 +41,14 @@
 
   ///// 4
   const usersWithMoreThan3Comments = usersFullInfomation.filter(
-    (u) => u.comments.length > 3
+    (user) => user.comments.length > 3
   );
 
   ///// 5
-  const usersReformatted = usersFullInfomation.map((u) => {
+  const usersReformatted = usersFullInfomation.map((user) => {
+    const { posts, comments } = user;
     return {
-      ...u,
+      ...user,
       commentsCount: comments.length,
       postsCount: posts.length,
     };
@@ -54,21 +65,14 @@
 
   ///// 7
   const usersSortedByPostsCount = usersReformatted.sort(
-    (a, b) => b.postsCount - a.postsCount
+    (userA, userB) => userB.postsCount - userA.postsCount
   );
 
   ///// 8
-  const post1Request = await fetch(
-    "https://jsonplaceholder.typicode.com/posts/1"
-  );
-  const commentsOfPost1Request = await fetch(
-    "https://jsonplaceholder.typicode.com/posts/1/comments"
-  );
-  const post1 = await post1Request.json();
-  const commentsOfPost1 = await commentsOfPost1Request.json();
-  post1.comments = commentsOfPost1;
+  const firstPost = await getPost(1);
 
   // console.dir(usersSortedByPostsCount, { depth: 3 });
-  // console.log(userWithMostPosts);
-  // console.log(post1);
+  // console.log(userWithMostComments);
+  console.log(firstPost);
+  console.log(usersReformatted);
 })();
